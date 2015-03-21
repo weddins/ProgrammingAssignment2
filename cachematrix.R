f259a7c35d675a9018a064f43ebf52b13e5267ad
weddins <weddins@ycp.edu>
2015-03-21 13:26:26 -0400

    updated cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,22 +6,22 @@
 ## 2) Cache the inverse object, x <- makeCacheMatrix(m)
 ## 3) Find and solve inverse, y <- cacheSolve(x)
 
-## This function takes a matrix as input and caches it using <<-
-## Thus it is a form of constructor function including sets & gets
+## This function takes a matrix as input and caches it
+## Thus it is a constructor function including sets & gets
 makeCacheMatrix <- function(x = matrix()) {
   # Invalidate m
-  m <- NULL
+  mtx <- NULL
   # Create setter function for te matrix
   set <- function(y)
   {
     x <<- y
-    m <<- NULL
+    mtx <<- NULL
   }
   # Create getter function matrix
   get <- function() x
   # Create getter/setter functions for the inverse function
-  setinverse <- function(inverse) m <<- inverse
-  getinverse <- function() m
+  setinverse <- function(inverse) mtx <<- inverse
+  getinverse <- function() mtx
   # Return a list with setters/getters
   list(set = set, 
        get = get, 
@@ -29,24 +29,25 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-## If the inverse matrix has been cached, this function pulls the inverse matrix from cache and returns it.
+## If the inverse matrix has been cached, this function pulls 
+## the inverse matrix from cache and returns it.
 ## If cache is not present, then it executes solve and sets the matrix.
 cacheSolve <- function(x, ...) {
   # Pull inverse matrix from cache
-  m <- x$getinverse()
+  mtx <- x$getinverse()
   # If inverse matrix is in the cashe, return it
-  if(!is.null(m)) {
+  if(!is.null(mtx)) {
     message("getting cached data")
-    return(m)
+    return(mtx)
   }
   # Else get the matrix
   data <- x$get()
   # Compute the inverse
-  m <- solve(data, ...)
+  mtx <- solve(data, ...)
   # Put the inverse into cache
-  x$setinverse(m)
+  x$setinverse(mtx)
   # Return the inverse
-  m
+  mtx
 }
 
 #Run functions automatically
